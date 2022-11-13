42a066b7b83be90e5ce5b81b93a5d22c0e4d06ae
Yerwts <104331179+Yerwts@users.noreply.github.com>
2022-11-13 23:17:00 +0000

    Nov 13, 2022, 6:17 PM

--- Assets/Script/RubyController.cs
+++ Assets/Script/RubyController.cs
@@ -160,7 +160,7 @@ public class RubyController : MonoBehaviour
         if (currentHealth <= 0)
         {
             LoseTextObject.SetActive(true);
-            audioSource.clip = winSound;
+            audioSource.clip = LoseSound;
             audioSource.Play();
             transform.position = new Vector3(-5f, 0f, -100f);
             speed = 0;
