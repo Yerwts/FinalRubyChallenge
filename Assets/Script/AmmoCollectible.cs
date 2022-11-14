645c0ca7a2c3784c0b2f55bedc0878f8b5a1e5c1
Yerwts <104331179+Yerwts@users.noreply.github.com>
2022-11-14 03:06:43 +0000

    Nov 13, 2022, 10:06 PM

--- Assets/Script/AmmoCollectible.cs
+++ Assets/Script/AmmoCollectible.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class AmmoCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if (controller.ammo <= controller.currentAmmo)
+            {
+                controller.ChangeAmmo(4);
+                controller.AmmoText();
+                Destroy(gameObject);
+
+
+            }
+        }
     }
 }
