3cf34f7d60479d89eb79c3f3934083a96ea2e292
Yerwts <104331179+Yerwts@users.noreply.github.com>
2022-11-14 03:56:27 +0000

    Nov 13, 2022, 10:56 PM

--- Assets/Script/AmmoCollectible.cs
+++ Assets/Script/AmmoCollectible.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class AmmoCollectible : MonoBehaviour
 {
     public AudioClip collectedClip;
-
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
