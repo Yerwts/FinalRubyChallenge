8aab224ffa46aaca353e632cf8571c881da330ae
Yerwts <104331179+Yerwts@users.noreply.github.com>
2022-12-01 16:22:28 +0000

    Dec 1, 2022, 11:22 AM

--- Assets/Script/ChocolateScript.cs
+++ Assets/Script/ChocolateScript.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChocolateScript : MonoBehaviour
+public class HealthCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioClip ChocoClip;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+
+                controller.PlaySound(collectedClip);
+            }
+        }
+
     }
 }
