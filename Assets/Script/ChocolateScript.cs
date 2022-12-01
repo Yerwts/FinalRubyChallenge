3d48559400bf6e1631621042c9fd1cd6f3491357
Yerwts <104331179+Yerwts@users.noreply.github.com>
2022-12-01 16:23:07 +0000

    Dec 1, 2022, 11:23 AM

--- Assets/Script/ChocolateScript.cs
+++ Assets/Script/ChocolateScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectible : MonoBehaviour
+public class CollectibleChocolate: MonoBehaviour
 {
     public AudioClip ChocoClip;
 
