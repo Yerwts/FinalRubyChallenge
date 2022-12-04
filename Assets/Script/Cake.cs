f8a57d404e8c2d62e47bcada9d953c7b1193fb9b
Yerwts <104331179+Yerwts@users.noreply.github.com>
2022-12-04 04:14:36 +0000

    Dec 3, 2022, 11:14 PM

---
+++ Assets/Script/Cake.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cake : MonoBehaviour
+{
+    public AudioClip CakeClip;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+
+
+            Destroy(gameObject);
+
+            controller.PlaySound(CakeClip);
+
+        }
+
+    }
+}
